d9efb4169b30e97600026dd9229691e4c3734b13
Alex-SK <aleksey.kozyura@gmail.com>
2020-08-14 23:11:02 +1000

    Add primitive types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,23 @@
-class HelloWorld {
-    static {
-        System.out.println("Hello world!");
-        System.exit(0);
+public class HelloWorld {
+
+    // Primitive types
+    byte b = 1;       //1 byte(256) from -128 to 127
+    short s = 129;      //2 bytes(6,5536) -32,768 to 32,767
+    int i = 646554;        //4 bytes(4,294,967,296) from -2,147,483,648 to 2,147,483,647
+    long l = 465535464;       //8 bytes(a lot) from -2^63 to 2^63-1
+    float f = 12.5778f;      //4 bytes. Sufficient for storing 6 to 7 decimal digits
+    double d = 3.78080808723;     //8 bytes. Sufficient for storing 15 decimal digits
+    boolean bool = true;
+    char c = 'c';
+
+    // Non-primitive types
+    //HelloWorld helloWorld = new HelloWorld(); // instance of class
+
+    public static void main(String[] args){
+
+        HelloWorld helloWorld1 = new HelloWorld(); // instance of class
+
+
+        System.out.println(helloWorld1.i);
     }
 }

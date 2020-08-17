70ad5ea2b839319cd1e019eb39ebd66ed9f15ec8
Alex-SK <aleksey.kozyura@gmail.com>
2020-08-17 21:56:03 +1000

    Add bad practice for access fields

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        
+        Person person = new Person();
+        System.out.println(person.name); // bad practice according to OOP
     }
 }

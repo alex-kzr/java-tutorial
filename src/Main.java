35ff5cf7c32dff01842645effb2d6089bbaf59ef
Alex-SK <aleksey.kozyura@gmail.com>
2020-08-19 01:02:04 +1000

    Add construtor examples

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+
+    int i;
+
+    // constructor can be private or public
+    Main(){
+        System.out.println("start constructor");
+    }
+
+    Main(int i){
+        this.i = i;
+        System.out.println(i);
+    }
+
+    Main(String s){
+        System.out.println(s);
+    }
+
     public static void main(String[] args){
-        
+        new Main();
+        new Main(5);
+        new Main("hello");
     }
 }

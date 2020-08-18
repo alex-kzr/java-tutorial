8988822f6787e1765d84820ca546640293e28816
Alex-SK <aleksey.kozyura@gmail.com>
2020-08-18 19:24:40 +1000

    Add some vars init examples

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+
+    int i; // init with 0
+    boolean bool; // init with false
+    Car car; // init with null
+
+
     public static void main(String[] args){
-        
+        int j; // compile error if we try read j
+        Car car2 = new Car();
+        System.out.println(car2.maxSpeed);
+        System.out.println(car2.main);
     }
+}
+
+class Car {
+    int maxSpeed;
+    Main main;
 }

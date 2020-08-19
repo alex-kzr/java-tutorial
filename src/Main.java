f23d1243865fad9b5e7fe21a24abebcc6c1b9ae8
Alex-SK <aleksey.kozyura@gmail.com>
2020-08-20 00:43:56 +1000

    Add enum examples

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
-    public static void main(String[] args){
-        
+
+    enum Color { RED, GREEN, BLUE}
+
+    public enum Size { SMALL(200), MEDIUM(300), BIG(400) {
+        @Override
+        public String getSizeClass(){
+            return "B";
+        }
+    };
+        int milliliters;
+        String sizeClass = "A";
+
+        Size(int ml){
+            this.milliliters = ml;
+        }
+        int getMilliliters() {
+            return milliliters;
+        }
+        String getSizeClass(){
+            return sizeClass;
+        }
     }
+
+    public static void main(String[] args){
+        Color color = Color.RED;
+        Size size1 = Size.MEDIUM;
+        Size size3 = Size.BIG;
+        System.out.println(color);
+        System.out.println(size1);
+        System.out.println(size1.milliliters);
+        System.out.println(size1.getSizeClass());
+        System.out.println(size3.getSizeClass());
+    }
+}
+
+
+
+// enums is something like this
+class Color2 {
+    public static final Color2 SMALL = new Color2();
+    public static final Color2 MEDIUM = new Color2();
+    public static final Color2 BIG = new Color2();
 }

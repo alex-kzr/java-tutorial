cf68ea1d03d2b3b90d9a639df84871910f0f9811
Alex-SK <aleksey.kozyura@gmail.com>
2020-08-19 18:10:08 +1000

    Add some array examples

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
+
+    int[] intArray = {5, 4, 3};
+    int intArray2[]; // bad practice
+    int[][] intArray3 = {{1, 2}, {3, 4}};
+    boolean[] boolArray = {true, false};
+    double[] doubles = {3.325, 42.425};
+
+
     public static void main(String[] args){
         
     }

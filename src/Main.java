6be9c2004ee34385e06484b3b2569d2702a8427f
Alex-SK <aleksey.kozyura@gmail.com>
2020-08-26 12:00:54 +1000

    Add some init examples

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
     int i; // init with 0
     boolean bool; // init with false
     Car car; // init with null
+    int y = 5, x, z; // init in one line
 
 
     public static void main(String[] args){
         int j; // compile error if we try read j
+        int y = 3; // will be used another y var on method level
         Car car2 = new Car();
         System.out.println(car2.maxSpeed);
         System.out.println(car2.main);

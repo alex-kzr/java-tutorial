ea78056db4c19cda384855a8af1ced089a80f457
Alex-SK <aleksey.kozyura@gmail.com>
2020-08-27 00:18:47 +1000

    Add comment examples

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args){
-        
+        // one line comment
+        /*
+        several lines comment
+         */
+        /**
+         * JavaDocs comment
+         *
+         * @author  unascribed
+         * @see     java.lang.Class
+         * @since   JDK1.0
+         */
     }
 }

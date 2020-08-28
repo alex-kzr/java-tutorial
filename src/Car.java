3d1dbbb930fda4ef486bd14026404f3e76fbf851
Alex-SK <aleksey.kozyura@gmail.com>
2020-08-28 13:12:20 +1000

    Add reference examples

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args){
-        
+
+        Car c1 = new Car();
+        Car c2 = c1;
+
+//        c1.setName("BMW");
+//        System.out.println("c2.getName() = " + c2.getName());
+//
+//        c2.setName("Toyota");
+//        System.out.println("c1.getName() = " + c1.getName() );
+
+        System.out.println("c1.getName() = " + c1.getName());
+        System.out.println("c1.getName() = " + c1.getName().length()); // java.lang.NullPointerException
+
+        c1 = null;
+
+        System.out.println("c1 = " c1.getName()); // java.lang.NullPointerException
+
     }
 }

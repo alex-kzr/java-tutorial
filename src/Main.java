5f36211bc74120307d28aa944ef34d7f3dc6477d
Alex-SK <aleksey.kozyura@gmail.com>
2020-08-28 12:33:19 +1000

    Add method call examples

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        method(1, 2.5f, true, new Object(), "hello", 234, 4342, 2424, 353, 131, 262);
-        System.out.println(method2());
+        // method(1, 2.5f, true, new Object(), "hello", 234, 4342, 2424, 353, 131, 262);
+        // System.out.println(method2());
+        // printString(getNewValue(getString()));
+        // printString(returnMethod());
+        // recursive(0);
+        // System.out.println(returnExample(-10));
+        returnExampleVoid(0);
     }
 
     static void method(int i, float f, boolean b, Object o, String s, int... j) {
@@ -17,5 +22,46 @@ public class Main {
 
     static Object method2(){
         return new Object();
+    }
+
+    public static String  getString() {
+        return "new string";
+    }
+
+    public static String getNewValue(String string) {
+        return string + " add new value!";
+    }
+
+    public static void printString(String string) {
+        System.out.println("value = " + string);
+    }
+
+    public static String returnMethod() {
+        return getNewValue("new value");
+    }
+
+    public static int recursive (int value) {
+        value += 1;
+        if (value == 10) {
+            return value;
+        }
+        System.out.println(value);
+
+        return recursive(value);
+    }
+
+    public static String returnExample (int count) {
+        if (count < 0) {
+            return "negative";
+        } else {
+            return  "positive";
+        }
+    }
+
+    public static void returnExampleVoid (int count) {
+        if (count == 0) {
+            return;
+        }
+        System.out.println(count);
     }
 }

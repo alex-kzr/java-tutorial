7c71f5ada3b082dd1e708b61044de20c3c262fc7
Alex-SK <aleksey.kozyura@gmail.com>
2020-08-28 11:50:23 +1000

    Add lib dependency

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import org.apache.commons.collections4.map.MultiKeyMap;
+
 public class Main {
     public static void main(String[] args){
-        
+        MultiKeyMap multiKeyMap = new MultiKeyMap();
     }
 }

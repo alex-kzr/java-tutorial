2a6b837837bf5ab9a231ab7acc0c0f07bf471aef
Alex-SK <aleksey.kozyura@gmail.com>
2020-08-31 14:39:57 +1000

    Add examples

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args){
-        
+
+        // call both conditions
+        if (condition1() & condition2()) {
+            System.out.println("ok");
+        }
+
+        // call only condition 1 (false) and exit
+        if (condition1() && condition2()) {
+            System.out.println("ok");
+        }
+
+        // call only condition 2 (true) and exit
+        if (condition2() || condition1()) {
+            System.out.println("ok");
+        }
+
+        // call both conditions
+        if (condition2() | condition1()) {
+            System.out.println("ok");
+        }
+    }
+
+    public static boolean condition1() {
+        System.out.println("condition 1");
+        return false;
+    }
+
+    public static boolean condition2() {
+        System.out.println("condition 2");
+        return true;
     }
 }

967bc0fd6c7ed80c5641c3b8750114f986e407cf
Alex-SK <aleksey.kozyura@gmail.com>
2020-09-09 17:44:48 +1000

    Add some enum examples

--- src/Main.java
+++ src/Main.java
@@ -23,14 +23,43 @@ public class Main {
     }
 
     public static void main(String[] args){
-        Color color = Color.RED;
-        Size size1 = Size.MEDIUM;
-        Size size3 = Size.BIG;
-        System.out.println(color);
-        System.out.println(size1);
-        System.out.println(size1.milliliters);
-        System.out.println(size1.getSizeClass());
-        System.out.println(size3.getSizeClass());
+//        Color color = Color.RED;
+//        Size size1 = Size.MEDIUM;
+//        Size size3 = Size.BIG;
+//        System.out.println(color);
+//        System.out.println(size1);
+//        System.out.println(size1.milliliters);
+//        System.out.println(size1.getSizeClass());
+//        System.out.println(size3.getSizeClass());
+
+        // get all enum values
+        for (int i = 0; i < GameLevel.values().length; i++){
+            System.out.println(GameLevel.values()[i]);
+        }
+
+        // for use in object
+        Game game = new Game();
+        game.setGameLevel(GameLevel.BEGINNER);
+
+        System.out.println("game.getGameLevel() = " + game.getGameLevel());
+
+        if (game.getGameLevel() == GameLevel.BEGINNER){
+            System.out.println(game.getGameLevel().getIndex() + " " + game.getGameLevel().getName());
+        }
+
+        switch (game.getGameLevel()) {
+            case BEGINNER:
+                System.out.println("beg");
+                break;
+            case PROFESSIONAL:
+                System.out.println("pro");
+                break;
+            case HARD:
+                System.out.println("hard");
+                break;
+            default:
+                System.out.println("other");
+        }
     }
 }
 

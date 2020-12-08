6829508fba732cb204aa202ada5859f677f688ec
Alex-SK <aleksey.kozyura@gmail.com>
2020-12-08 11:05:10 +1000

    Add example using java.net.HttpUrlConnection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
+
+    private static HttpURLConnection connection;
+
     public static void main(String[] args){
-        
+
+        // Method 1: java.net.HttpUrlConnection - java 8
+
+        BufferedReader reader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
+
+        try {
+            URL url = new URL("https://jsonplaceholder.typicode.com/albums");
+
+            connection = (HttpURLConnection) url.openConnection();
+
+            // Request setup
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            int status = connection.getResponseCode();
+
+            if (status > 299 ) {
+
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                while ((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+            } else {
+
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                while ((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            }
+
+            System.out.println(responseContent.toString());
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            connection.disconnect();
+        }
+
     }
 }

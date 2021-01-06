86b2bd36482e3f45e4529222a40ca275cf765eec
Alex-SK <aleksey.kozyura@gmail.com>
2021-01-06 14:17:13 +1000

    Add read input file example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import javafx.scene.shape.Path;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        
+        File textFile = new File("file.txt");
+
+        try {
+            Scanner scanner = new Scanner(textFile);
+
+            int lineNumber = 1;
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                System.out.println("line " + lineNumber + " :" + line);
+                lineNumber++;
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }

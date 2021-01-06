5f801c5268dd4a9552b7ad35ea98c2efb7505f36
Alex-SK <aleksey.kozyura@gmail.com>
2021-01-06 14:02:50 +1000

    Add example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import okhttp3.*;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args){
-        
+
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
     }
+
+    public static void main(String[] args) throws IOException {
+
+        Main getExample = new Main();
+        String response = getExample.run("https://raw.github.com/square/okhttp/master/README.md");
+        System.out.println(response);
+    }
+
 }

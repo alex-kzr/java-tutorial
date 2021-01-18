6e77a83b01220b09aa85ddf66a1ac6f2ae110673
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-01-18 12:50:05 +1000

    Add MVC sample

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,69 @@
 public class Main {
     public static void main(String[] args){
-        
+        Controller controller = new Controller();
+        controller.execute();
+    }
+}
+
+class Controller {
+    ModelLayer modelLayer = new DBLayer();
+    View view = new ConsoleView();
+
+    void execute() {
+        Student student = modelLayer.getStudent();
+        view.showStudent(student);
+    }
+}
+
+interface ModelLayer {
+    Student getStudent();
+}
+
+class DBLayer implements ModelLayer {
+
+    @Override
+    public Student getStudent() {
+        return new Student();
+    }
+}
+
+class FSLayer implements ModelLayer {
+
+    @Override
+    public Student getStudent() {
+        return new Student();
+    }
+}
+
+class Student {
+    String name = "Alex";
+    int age = 20;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+interface View {
+    void showStudent (Student student);
+}
+
+class ConsoleView implements View {
+
+    @Override
+    public void showStudent(Student student) {
+        System.out.println("Student name: " + student.getName() + " age: " + student.getAge());
     }
 }

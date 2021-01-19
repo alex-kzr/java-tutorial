1d32a5ed09a474d39e2278de1f4c171f4c933b3a
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-01-19 14:29:01 +1000

    Add singleton example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args){
-        
+
+        final int ARRAY_SIZE = 1000;
+        Singleton array[] = new Singleton[ARRAY_SIZE];
+
+        for (int i = 0; i<ARRAY_SIZE; i++) {
+            array[i] = Singleton.getInstance();
+        }
+
+        System.out.println(Singleton.getCounter());
+    }
+}
+
+class Singleton {
+
+    private static Singleton instance;
+
+    private static int counter = 0;
+
+    private Singleton(){
+        counter++;
+    }
+
+    public static Singleton getInstance(){
+
+        if (instance == null) {
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+
+    public static int getCounter() {
+        return counter;
     }
 }

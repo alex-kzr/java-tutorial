7e22c245975e16befa84ab666259f180c3542790
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-07 14:44:33 +1000

    Add my annotation and annotation use examples

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
+@MyAnnotation
 public class Main {
+    @MyAnnotation
+    private String name;
+
+    @MyAnnotation
+    public void myMethod1() {
+
+    }
+
+    @MyAnnotation
+    public void myMethod2(@MyAnnotation int value) {
+        @MyAnnotation String localVar = "Hello";
+    }
+
+    MyAnnotation
     public static void main(String[] args){
         
     }

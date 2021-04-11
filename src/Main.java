0e7fc1cf0d9efb0f6d72b35d04d2d3ec9e11a5e6
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-11 09:29:37 +0300

    Add samples: class implementation, anonymos interface implementation and lambda

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+interface Executable {
+    void execute();
+}
+
+class Runner {
+    public void run(Executable executable) {
+        executable.execute();
+    }
+}
+
+class ExecutableImplementation implements Executable {
+    @Override
+    public void execute() {
+        System.out.println("Execute from declared class");
+    }
+}
+
 public class Main {
     public static void main(String[] args){
-        
+        Runner runner = new Runner();
+
+        runner.run(new ExecutableImplementation());
+
+        runner.run(new Executable() {
+            @Override
+            public void execute() {
+                System.out.println("Execute from anonymous interface implementation");
+            }
+        });
+
+        runner.run(() -> System.out.println("Execute from lambda"));
     }
 }

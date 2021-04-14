7e4100e620b6443c689b814ab7a37a6b6fb9839a
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-14 12:49:18 +1000

    Add error declaring vars in the lambda scope

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,19 @@ public class Main {
     public static void main(String[] args){
         Runner runner = new Runner();
 
+        int a = 1;
         runner.run(new Executable() {
             @Override
             public int execute(int x, int y) {
+                int a = 2;
                 return x + y;
             }
         });
 
-        runner.run((x, y) -> y - x);
+        runner.run((x, y) -> {
+            // int a = 3; - error because lambda has same scope as external caller
+            int b = 4;
+            return y - x;
+        });
     }
 }

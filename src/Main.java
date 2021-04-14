bb1ba27dea55dac06dcb729b2460996e4b446c28
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-14 13:09:32 +1000

    Create implementation using lambda

--- src/Main.java
+++ src/Main.java
@@ -22,10 +22,10 @@ public class Main {
             }
         });
 
-        runner.run((x, y) -> {
-            // int a = 3; - error because lambda has same scope as external caller
-            int b = 4;
+        Executable executable = (x, y) -> {
             return y - x;
-        });
+        };
+
+        runner.run(executable);
     }
 }

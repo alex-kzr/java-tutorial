6c26b74ac2ba1eb71539a4f13faf98876d16fae0
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-14 13:02:20 +1000

    Refactor using lambda

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,10 @@ public class Main {
         list.add("a");
         list.add("b");
 
-        list.sort(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if (o1.length() > o2.length()) {
-                    return 1;
-                } else if (o1.length() < o2.length()) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            }
+        list.sort((s1, s2) -> {
+           if (s1.length() > s2.length()) return 1;
+           else if (s1.length() < s2.length()) return -1;
+           else return 0;
         });
 
         System.out.println(list);

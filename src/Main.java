83f37bfb452a2801d12ed4dd2f31a704c6b68b5f
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-14 13:23:34 +1000

    Add array and list set sample

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-        
+        int[] arr = new int[10];
+        List<Integer> list = new ArrayList<>();
+
+        fillArr(arr);
+        fillList(list);
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(list);
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = arr[i] * 2;
+            list.set(i, list.get(i) * 2);
+        }
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(list);
+    }
+
+    private static void fillArr(int[] arr) {
+        for (int i = 0; i < 10; i++) {
+            arr[i] = i + 1;
+        }
+    }
+
+    private static void fillList(List<Integer> list) {
+        for (int i = 0; i < 10; i++) {
+            list.add(i + 1);
+        }
     }
 }
